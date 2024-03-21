25990842b51be82eab2a093bc00818747bcfd2a6
ando-ands <ands.alvarado@gmail.com>
2024-03-21 17:15:56 -0600

    Primeros Cambios

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,11 @@ import TermsAndConditions from './components/TermsAndConditions';
 import SubmitButton from './components/SubmitButton';
 import MiComponente from './components/contador';
 
-function App() {
+
+
+{/* <Provider store= {store}></Provider> */}
+
+const App = () => {
   return (
     <div className="App row">
       <h1>Check-out</h1>
@@ -17,6 +21,7 @@ function App() {
       <MiComponente />
     </div>
   );
-}
+};
 
 export default App;
+
--- src/components/UserData.jsx
+++ src/components/UserData.jsx
@@ -29,4 +29,3 @@ function UserData() {
 
 export default UserData;
 
-
